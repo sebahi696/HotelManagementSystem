52b06a2a43cc1f3655f2c0d018c7b4f02d5108b3
sebahi696 <78598861+sebahi696@users.noreply.github.com>
2024-05-23 20:07:24 +0300

    Create Room.cs

---
+++ Room.cs
@@ -0,0 +1,12 @@
+namespace HotelManagementSystem
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int Floor { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; }
+        public int Beds { get; set; }
+    }
+}
