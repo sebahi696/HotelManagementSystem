bef7390c66c742774cbe46d204149766fca4682d
sebahi696 <78598861+sebahi696@users.noreply.github.com>
2024-05-23 20:08:05 +0300

    Create Bill.cs

---
+++ Bill.cs
@@ -0,0 +1,11 @@
+namespace HotelManagementSystem
+{
+    public class Bill
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
+
